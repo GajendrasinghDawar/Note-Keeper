ff73f57a46a6a9c82c4c7193ed4fd63a6edb5c5c
GajendrasinghDawar <gajendrasinghdawar@gmail.com>
2024-09-18 17:26:26 +0530

    see sw working offline

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,30 @@
 importScripts("/cache-list.js")
 
+const CACHE_NAME = 'my-cache-01';
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-cache').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/',
                 ...urlsToCache,
             ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [ CACHE_NAME ];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName))
+                    {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
