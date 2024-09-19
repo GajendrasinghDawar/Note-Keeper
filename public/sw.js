ee74d87ce0ca9b1a1646c8c84ac2820d5326e892
GajendrasinghDawar <gajendrasinghdawar@gmail.com>
2024-09-19 22:19:25 +0530

    add routing 08

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/cache-list.js")
+importScripts("/cache-list.js");
 
 const CACHE_NAME = 'my-cache-01';
 
@@ -30,12 +30,30 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request).catch((error) => {
-                console.error('Fetch failed; returning offline page instead.', error);
-                return caches.match('/offline.html'); 
-            });
-        })
-    );
+    if (event.request.mode === 'navigate')
+    {
+        event.respondWith(
+            caches.match('/').then((response) => {
+                return response || fetch(event.request).then((fetchResponse) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, fetchResponse.clone());
+                        return fetchResponse;
+                    });
+                }).catch((error) => {
+                    console.error('Fetch failed; returning offline page instead.', error);
+                    return caches.match('/offline.html');
+                });
+            })
+        );
+    } else
+    {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request).catch((error) => {
+                    console.error('Fetch failed; returning offline page instead.', error);
+                    return caches.match('/offline.html');
+                });
+            })
+        );
+    }
 });
